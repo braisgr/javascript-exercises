44c36afbc8e4fef7b2528ab380e66194c76b46a6
braisgr <bgruibal@gmail.com>
2024-02-06 19:45:52 +0100

    First 7 exercises solved

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+  const filteredArray = [];
+  for(let i=0; i<array.length; i++){
+    if(!args.includes(array[i])){
+      filteredArray.push(array[i]);
+    }
+  }
+  return filteredArray;
 };
 
 // Do not edit below this line
